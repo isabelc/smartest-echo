959e70413505ae8974238a3a314e9c7580f4a0aa
Isabel Castillo <me@isabelcastillo.com>
2015-01-14 09:56:16 -0500

    remove unsed files and IE6 support

--- functions.php
+++ functions.php
@@ -1,8 +1,13 @@
 <?php require_once TEMPLATEPATH .'/isa_framework/functions.php';
-$func_path = TEMPLATEPATH . '/functions/';
-$inc_path = TEMPLATEPATH . '/includes/';
-require_once $func_path . 'admin-init.php';
-require_once $func_path . 'storefront-functions.php';
-require_once $inc_path . 'theme-options.php'; 
-require_once $inc_path . 'theme-functions.php';
-require_once $inc_path . 'theme-js.php'; ?>
+$func = TEMPLATEPATH . '/functions/';
+$inc = TEMPLATEPATH . '/includes/';
+
+
+// require_once $func . 'admin-init.php'; @test ebed it here...
+require_once $func . 'admin-setup.php';
+require_once $func . 'admin-interface.php';
+require_once $func . 'storefront-functions.php';
+
+require_once $inc . 'theme-options.php'; 
+require_once $inc . 'theme-functions.php';
+require_once $inc . 'theme-js.php'; ?>
--- functions/admin-init.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$functions_path = TEMPLATEPATH . '/functions/';
-require_once ($functions_path . 'admin-setup.php');
-require_once ($functions_path . 'admin-interface.php');
-require_once ($functions_path . 'storefront-functions.php');
-?>
