2b91251a55c8333641d2ff275480decba53878e0
Isabel Castillo <me@isabelcastillo.com>
2015-01-20 11:01:29 -0500

    remove admin-interface and admin-style

--- functions.php
+++ functions.php
@@ -4,8 +4,18 @@ $inc = TEMPLATEPATH . '/includes/';
 
 
 // require_once $func . 'admin-init.php'; @test ebed it here...
-require_once $func . 'admin-interface.php';
+
 require_once $func . 'storefront-functions.php';
 
 require_once $inc . 'theme-functions.php';
-require_once $inc . 'theme-js.php'; ?>
+require_once $inc . 'theme-js.php'; 
+
+
+
+// @test
+
+$opt = get_option('smartestb_options');
+
+isa_log($opt);
+
+?>
